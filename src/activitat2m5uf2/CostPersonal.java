9f1e0c9873848a4a883f004063eb46726518edce
davidblancolopez <daviddbl94@gmail.com>
2016-10-21 19:52:20 +0200

    Cambios

--- src/activitat2m5uf2/CostPersonal.java
+++ src/activitat2m5uf2/CostPersonal.java
@@ -5,20 +5,22 @@ import java.util.Collection;
 
 public class CostPersonal {
 /**
- * Este metodo recorre el array de trabajadores y va comprobando uno a uno 
+ * Este metodo recorre el array de trabajadores y va comprobando uno a uno si 
+ * ese trabajador es director o subdirector o es un trabajador normal, en funcion 
+ * de la condición que cumpla se calculara costFinal. Si es director o subdirector
+ * será costFinal + la nomina, si es trabajador normal será costFinal + la nomina + horesExtres.
  * @param treballadors
  * @return 
  */
-	static float CostDelPersonal(Treballador treballadors[]) {
+    public static float CostDelPersonal(Treballador treballadors[]) {
 		float costFinal = 0;
 		Treballador treballador;
 		
 		for (int i = 0; i < treballadors.length; i++) {
-			treballador = treballadors[i];
 			
-			if (treballador.getTipusTreballador() == Treballador.DIRECTOR || \
-			treballador.getTipusTreballador() == Treballador.SUBDIRECTOR) {
-				costFinal = costFinal + treballador.getNomina();
+			if (treballadors[i].getTipusTreballador() == Treballador.DIRECTOR || \
+			treballadors[i].getTipusTreballador() == Treballador.SUBDIRECTOR) {
+				costFinal = costFinal + treballadors[i].getNomina();
 			} else {
 				costFinal = costFinal + treballador.getNomina() + \
 					(treballador.getHoresExtres() * 20);
