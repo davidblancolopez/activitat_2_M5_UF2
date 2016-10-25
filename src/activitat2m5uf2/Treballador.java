5b5cf71f921996621ba610fa9c391676822a1166
davidblancolopez <daviddbl94@gmail.com>
2016-10-25 20:26:14 +0200

    Cambios

--- src/activitat2m5uf2/Treballador.java
+++ src/activitat2m5uf2/Treballador.java
@@ -3,16 +3,9 @@ package activitat2m5uf2;
 
 
 public class Treballador {
-    String TipusTreballador;
+    
 
     
-    
-    
-    
-    
-    
-    public String getTipusTreballador() {
-        return TipusTreballador;
-    }
+
 
 }
